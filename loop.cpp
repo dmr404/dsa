1f54ff3c139c33f5f0d1d196af72c08d16f1859d
dmr404 <divyam.rai10may@gmail.com>
2026-02-05 21:17:12 +0530

    loopy loopy loop

--- loop.cpp
+++ loop.cpp
@@ -3,7 +3,31 @@ using namespace std;
 
 int main() {
     for(int i = 0; i < 5; i++) {
-        cout << "I am mastering Git!" << endl;
+        cout <<i+99<<"  ";
     }
+    cout<<endl;
+    cout<<endl;
+
+    //reverse of a no. by while loop
+    int revDig = 0;
+    int n = 146454;
+    while(n>0) {
+        int lastDig = n%10;
+        revDig = lastDig + revDig*10;
+        n=n/10;
+    }
+    cout<<revDig<<endl<<endl;
+
+    //do-while loop, works get done atleast once
+    int i = 1;
+    do{
+        cout<<i<<"  ";
+        i++;
+    } while(i<=5);
+    cout<<endl<<endl;
+
+
+
+
     return 0;
 }
