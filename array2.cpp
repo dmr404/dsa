821c7280f7253221fcd83cc5f93ef180b4779bf0
dmr404 <divyam.rai10may@gmail.com>
2026-02-07 22:33:50 +0530

    array basic 2

--- array2.cpp
+++ array2.cpp
@@ -1,7 +1,7 @@
 //largest in array
 #include<iostream>
 using namespace std;
-int main() {
+/*int main() {
     int arr[] = {2,3,4,4};
     int n = sizeof(arr)/sizeof(int);
     int max = arr[0];
@@ -14,4 +14,18 @@ int main() {
     }
     cout<<max<<endl;
 return 0;
+}*/
+
+//array pased by refrence
+void printArr(int nums[], int n) {
+    for(int i=0; i<n; i++) {
+        cout<<nums[i]<<",";
+    }
+    cout<<endl;
+}
+int main() {
+    int arr[]={1,2,3,4,5} ;
+    int n = sizeof(arr)/sizeof(int);
+    printArr(arr, n);
+    return 0;
 }
