e0ca80a28f73a3311f2217bfddbf256e89b93b15
dmr404 <divyam.rai10may@gmail.com>
2026-02-07 22:53:24 +0530

    linear search and reverse ig=f on an array

---
+++ linearSearch.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+//LINEAR SEARCH : to find index value of key
+/*int linearSearch(int *arr, int n, int key) {
+    for(int i = 0; i<n; i++) {
+        if(arr[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+int main() {
+    int arr[] = {2,4,6,8,10,12,14,16} ;
+    int n = sizeof(arr)/sizeof(int);
+    cout<<linearSearch(arr, n, 10)<<endl;
+    return 0;
+}*/
+
+//REVERSE OF AN ARRAY
+//1)with extra space (copy space bnaya)
+void printArr(int *arr, int n) {
+    for(int i = 0; i<=n-1; i++) {
+        cout<<arr[i]<<",";
+    }
+    cout<<endl;
+}
+int main() {
+    int arr[] = {5,4,3,2,9};
+    int n = sizeof(arr) / sizeof(int);
+    
+    int copyArr[n];
+    for(int i = 0; i<n; i++) {
+        int j = n-i-1;
+        copyArr[i] = arr[j];
+    }
+    for(int i = 0; i<n; i++) {
+        arr[i] = copyArr[i];
+    }
+    printArr(arr, n);
+}
