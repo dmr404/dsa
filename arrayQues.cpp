c2d53c2fd2e07ee4eea59bbafb038a115e915d52
dmr404 <divyam.rai10may@gmail.com>
2026-02-10 23:20:14 +0530

    arrays imp questions

---
+++ arrayQues.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+//1) Given an integer array nums,return true if any value appears at least 
+//twice in the array,and return false if every element is distinct.
+
+/*nt distinct(int *arr, int n) {
+    for(int i = 0; i<n; i++) {
+        for(int j = 0; j<i; j++) {
+            if(arr[j]==arr[i]) {
+                cout<<"true";
+                cout<<arr[j];
+            }
+        }
+    }
+    
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5,6,3,7,8,8};
+    int n = sizeof(arr) / sizeof(int);
+    distinct(arr, n);
+
+    return 0;
+}*/
+
