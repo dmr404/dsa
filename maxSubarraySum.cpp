c86ec662db5190f0875b27b091518a7255144d3d
dmr404 <divyam.rai10may@gmail.com>
2026-02-10 18:59:04 +0530

    3ways to do it

--- maxSubarraySum.cpp
+++ maxSubarraySum.cpp
@@ -27,9 +27,38 @@ void maxSubarraySum1(int *arr, int n) {
     }
     cout<<"maximum subarray sum = "<<maxSum;
 }
+
+//OPTIMIZED WAY
+void maxSubarraySum2(int *arr, int n) {
+    int maxSum =INT8_MIN;
+    for(int start =0; start<n; start++) {
+        int currSum = 0;
+        for(int end = start; end<n; end++) {
+            currSum += arr[end];
+            maxSum= max(maxSum, currSum);
+        }
+        
+    }
+    cout<<"maximum subarray sum = "<<maxSum;
+}
+
+//KADANE'S ALGORITHM
+void maxSubarraySum3(int *arr, int n) {
+    int maxSum =INT8_MIN;
+    int currSum = 0;
+    for(int i =0; i<n; i++) {
+        currSum += arr[i];
+        maxSum = max(currSum, maxSum);
+        if(currSum<0) {
+            currSum=0;
+        }
+        
+    }
+    cout<<"maximum subarray sum = "<<maxSum;
+}
 int main() {
     int arr[] = {-2,4,-6,8,10};
     int n = sizeof(arr)/sizeof(int);
-    maxSubarraySum1(arr, n);
+    maxSubarraySum3(arr, n);
 return 0;
 }
