76e1385ef0800177ebbf65ed78f8d4e4f49c9a94
dmr404 <divyam.rai10may@gmail.com>
2026-02-17 22:23:29 +0530

    bubble sort

---
+++ bubble_sort.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+/*int main() {
+    int arr[] = {5,4,1,2,3};
+    int n = sizeof(arr) / sizeof(int);
+    for(int i =0; i<n-1; i++) {
+        for(int j = 0; j<n-i-1; j++) {
+            if(arr[j]>arr[j+1])
+            swap(arr[j], arr[j+1]);
+        }
+    }
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+}*/
+
