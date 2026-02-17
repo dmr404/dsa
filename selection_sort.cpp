62f1bfeb8bb171da9d4fb38cd2270e7c57d86294
dmr404 <divyam.rai10may@gmail.com>
2026-02-17 22:49:22 +0530

    selection array

---
+++ selection_sort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5,4,1,2,3};
+    int n = sizeof(arr) / sizeof(int);
+    for(int i = 0; i<n-1; i++) {
+        int minIdx = i;
+        for(int j = i+1;j<n;j++) {
+            if(arr[j]<arr[minIdx])
+            minIdx = j;
+        }
+        swap(arr[i],arr[minIdx]);
+
+    }
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+    
+}
