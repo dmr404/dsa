5ee1d681adf6b233393b1af29f38bf300bfb7689
dmr404 <divyam.rai10may@gmail.com>
2026-02-18 23:33:02 +0530

    not done

--- insertion_array.cpp
+++ insertion_array.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+/*int main() {
     int arr[] = {5,4,1,2,3};
     int n = sizeof(arr) / sizeof(int);
     for(int i =1; i<n; i++) {
@@ -18,5 +18,35 @@ int main() {
     for(int i=0; i<n; i++) {
         cout<<arr[i]<<" ";
     }
+    return 0;
+}*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10] = {10, 20, 30, 40, 50}; // initial array
+    int n = 5; // current size
+    int pos, elem;
+
+    cout << "Enter position (1-based index): ";
+    cin >> pos;
+    cout << "Enter element to insert: ";
+    cin >> elem;
+
+    // Shift elements to the right
+    for (int i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos - 1] = elem; // insert element
+    n++; // increase size
+
+    cout << "Array after insertion: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
