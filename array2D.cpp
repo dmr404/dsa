6823019080c35dfa6a9b80f5fdaf05258c47edec
dmr404 <divyam.rai10may@gmail.com>
2026-02-22 18:29:47 +0530

    2d array start

---
+++ array2D.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+/*int main() {
+    int stud[3][5];
+    int n=3, m=5;
+    for(int i = 0; i<n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout<<"enter ur array \n";
+            cin>>stud[i][j];
+        }
+    }
+    for(int i = 0; i<n; i++) {
+        for(int j = 0; j < m; j++) {
+            
+            cout<<stud[i][j];
+        }
+    }
+}*/
+
